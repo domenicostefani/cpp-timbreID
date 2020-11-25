f588f86966e27f326961f77c4276b185b0db5fe1
Domenico Stefani <domenico.stefani@studenti.unitn.it>
2020-11-25 16:35:24 +0100

    minor

--- Demos/Demo-guitarTimbreClassifier/Builds/LinuxMakefile/copyToElk.sh
+++ Demos/Demo-guitarTimbreClassifier/Builds/LinuxMakefile/copyToElk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-scp ~/Develop/cpp-timbreID/Demos/Demo-guitarTimbreClassifier/Builds/LinuxMakefile/build/Demo-guitarTimbreClassifier.so root@10.42.0.47:/home/mind/GuitarTimbreClassifier/
+scp ~/Develop/cpp-timbreID/Demos/Demo-guitarTimbreClassifier/Builds/LinuxMakefile/build/Demo-guitarTimbreClassifier.so root@elk-pi.local:/home/mind/GuitarTimbreClassifier/
 
